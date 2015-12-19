6c00769efcac2e700b65d99240a228640e440c13
fforres <felipe.torressepulveda@gmail.com>
2015-12-19 17:03:41 -0300

    adding changes

--- src/redux/modules/coworks.js
+++ src/redux/modules/coworks.js
@@ -42,7 +42,6 @@ export const actions = {
 // ------------------------------------
 // Reducer
 // ------------------------------------
-
 const initialState = {
   coworks: {},
   coworksFiltered: {},
