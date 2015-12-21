a176db5c6faf661ea926472ef675d797df5d4bb7
fforres <felipe.torressepulveda@gmail.com>
2015-12-20 22:14:06 -0300

    adding readme, new version 1.5 , nugget de usuario, sesiones, y persisitencia de Store

--- src/app.js
+++ src/app.js
@@ -5,22 +5,27 @@ import { syncReduxAndRouter } from 'redux-simple-router';
 import routes                 from './routes';
 import Root                   from './containers/Root';
 import configureStore         from './redux/configureStore';
+import { getStoredState, autoRehydrate, persistStore } from 'redux-persist';
 
-const history = createBrowserHistory();
-const store   = configureStore(window.__INITIAL_STATE__, __DEBUG__);
-syncReduxAndRouter(history, store, (state) => state.router);
-
-// We render the DevTools window here rather than in the Root component
-// because we need access to the store but want this logic to be removed via
-// uglification and dead code removal when __DEBUG_NW__ is false, which
-// wouldn't be possible if it was handled via a React component prop.
-if (__DEBUG__ && __DEBUG_NW__) {
-  require('utils/createDevToolsWindow').default(store);
-}
-require('utils/firebase/firebaseReduxSubscriber').default(store);
-
+const persistConfig = {
+  skipRestore: true
+};
 // Render the React application to the DOM
-ReactDOM.render(
-  <Root history={history} routes={routes} store={store} />,
-  document.getElementById('root')
-);
+getStoredState(persistConfig, (err, initialState) => {
+  const history = createBrowserHistory();
+  const store   = configureStore(initialState, __DEBUG__);
+  syncReduxAndRouter(history, store, (state) => state.router);
+  // We render the DevTools window here rather than in the Root component
+  // because we need access to the store but want this logic to be removed via
+  // uglification and dead code removal when __DEBUG_NW__ is false, which
+  // wouldn't be possible if it was handled via a React component prop.
+  if (__DEBUG__ && __DEBUG_NW__) {
+    require('utils/createDevToolsWindow').default(store);
+  }
+  require('utils/firebase/firebaseReduxSubscriber').default(store);
+  persistStore(store, persistConfig);
+  ReactDOM.render(
+    <Root history={history} routes={routes} store={store} />,
+    document.getElementById('root')
+  );
+});
