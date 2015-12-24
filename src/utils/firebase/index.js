5a210f9e319e8aea037b352747c94a72b0c2548b
fforres <felipe.torressepulveda@gmail.com>
2015-12-24 19:10:16 -0300

    almost there, lacking the dispatcher

--- src/utils/firebase/index.js
+++
@@ -1,3 +0,0 @@
-export firebaseReduxSubscriber    from './firebaseReduxSubscriber.js';
-export fireBase                   from './listener.js';
-export firebaseComponent          from './firebaseComponent.js';
